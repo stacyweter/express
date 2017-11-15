097a3a2bc2a04a34741a95e7923a08d903f1c270
Stacy Weter <sw0113896@otc.edu>
2017-11-14 18:07:02 -0600

    updated code

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.get('/about.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'))
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
